9aa3d6e8be3725814169d2b5cd3b37f3dfb41316
Iain Johnson <iainj@gaelquality.com>
2011-12-14 17:23:10 +0000

    Adding dummy job controller and view

---
+++ src/JobSystem.Mvc/Controllers/JobController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobSystem.Mvc.Controllers
+{
+    public class JobController : Controller
+    {
+        //
+        // GET: /Job/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
