c39cc19869a08a2c4deca44bd4774acb0f2cc2b9
Chris O'Neil <chriso@gaelquality.com>
2011-12-30 15:08:13 +0000

    Added lazy loading for the job get.

--- src/JobSystem.DataAccess.NHibernate/Repositories/JobRepository.cs
+++ src/JobSystem.DataAccess.NHibernate/Repositories/JobRepository.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using JobSystem.DataModel.Entities;
 using JobSystem.DataModel.Repositories;
+using NHibernate;
 using NHibernate.Linq;
 
 namespace JobSystem.DataAccess.NHibernate.Repositories
 {
 	public class JobRepository : RepositoryBase<Job>, IJobRepository
 	{
+		public override Job GetById(Guid id)
+		{
+			var job = CurrentSession.Get<Job>(id);
+			NHibernateUtil.Initialize(job.Customer);
+			NHibernateUtil.Initialize(job.Type);
+			NHibernateUtil.Initialize(job.CreatedBy);
+			return job;
+		}
+
 		public IEnumerable<Job> GetApprovedJobs()
 		{
 			return CurrentSession.Query<Job>().Where(j => !j.IsPending);
