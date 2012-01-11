61f261764e55a7a6d150e46019d10352a6620276
Chris O'Neil <chriso@gaelquality.com>
2012-01-11 09:43:01 +0000

    Added transaction to instrument controller.

--- src/JobSystem.Mvc/Controllers/InstrumentController.cs
+++ src/JobSystem.Mvc/Controllers/InstrumentController.cs
@@ -5,11 +5,12 @@ using JobSystem.BusinessLogic.Services;
 using JobSystem.BusinessLogic.Validation.Core;
 using JobSystem.Mvc.Core.UIValidation;
 using JobSystem.Mvc.ViewModels.Instruments;
+using JobSystem.DataAccess.NHibernate.Web;
 
 namespace JobSystem.Mvc.Controllers
 {
-    public class InstrumentController : Controller
-    {
+	public class InstrumentController : Controller
+	{
 		private readonly InstrumentService _instrumentService;
 
 		public InstrumentController(InstrumentService instrumentService)
@@ -17,24 +18,25 @@ namespace JobSystem.Mvc.Controllers
 			_instrumentService = instrumentService;
 		}
 
-        public ActionResult Index()
-        {
-           var instruments = _instrumentService.GetInstruments().Select(
-				i => new InstrumentViewModel
-				{
-					Id = i.Id.ToString(),
-					Description = i.Description,
-					Manufacturer = i.Manufacturer,
-					ModelNo = i.ModelNo,
-					Range = i.Range
-				}).ToList();
+		public ActionResult Index()
+		{
+			var instruments = _instrumentService.GetInstruments().Select(
+				 i => new InstrumentViewModel
+				 {
+					 Id = i.Id.ToString(),
+					 Description = i.Description,
+					 Manufacturer = i.Manufacturer,
+					 ModelNo = i.ModelNo,
+					 Range = i.Range
+				 }).ToList();
 			var viewModel = new InstrumentListViewModel();
 			viewModel.Instruments = instruments;
 			viewModel.CreateViewModel = new InstrumentViewModel();
 			return View(viewModel);
-        }
+		}
 
 		[HttpPost]
+		[Transaction]
 		public ActionResult Create(InstrumentViewModel viewModel)
 		{
 			if (ModelState.IsValid)
@@ -52,6 +54,5 @@ namespace JobSystem.Mvc.Controllers
 			}
 			return View(viewModel);
 		}
-
-    }
+	}
 }
