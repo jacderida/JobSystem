f7c7906430ed45727737b3f9b8de0729b3a39258
Chris O'Neil <chriso@gaelquality.com>
2012-01-25 22:31:01 +0000

    Added versioning.

--- src/JobSystem.Mvc/Properties/AssemblyInfo.cs
+++ src/JobSystem.Mvc/Properties/AssemblyInfo.cs
@@ -8,10 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JobSystem.Mvc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("JobSystem.Mvc")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +27,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
