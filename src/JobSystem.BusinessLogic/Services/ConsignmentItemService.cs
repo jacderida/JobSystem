ab854ae98e17fc01a2289c543170c67ab52ca319
Chris O'Neil <chriso@gaelquality.com>
2012-02-09 19:07:53 +0000

    Commit before branch.

--- src/JobSystem.BusinessLogic/Services/ConsignmentItemService.cs
+++ src/JobSystem.BusinessLogic/Services/ConsignmentItemService.cs
@@ -10,25 +10,22 @@ namespace JobSystem.BusinessLogic.Services
 {
 	public class ConsignmentItemService : ServiceBase
 	{
-		private readonly ListItemService _listItemService;
 		private readonly ItemHistoryService _itemHistoryService;
 		private readonly IConsignmentRepository _consignmentRepository;
 		private readonly IConsignmentItemRepository _consignmentItemRepository;
-		
 
 		public ConsignmentItemService(
-			JobItemService jobItemRepository,
 			ItemHistoryService itemHistoryService,
-			ListItemService listItemService,
 			IUserContext applicationContext,
 			IConsignmentRepository consignmentRepository,
 			IConsignmentItemRepository consignmentItemRepository,
 			IQueueDispatcher<IMessage> dispatcher) : base(applicationContext, dispatcher)
 		{
-			_listItemService = listItemService;
 			_itemHistoryService = itemHistoryService;
 			_consignmentRepository = consignmentRepository;
 			_consignmentItemRepository = consignmentItemRepository;
 		}
+
+
 	}
 }
