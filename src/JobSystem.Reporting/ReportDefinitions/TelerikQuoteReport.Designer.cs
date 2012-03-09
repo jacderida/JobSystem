fde719c5b7108ef14067c97ad61f0714b3ba5591
Chris O'Neil <chriso@gaelquality.com>
2012-03-09 21:20:57 +0000

    Quote reports.
    Added a template report with the report data source.

---
+++ src/JobSystem.Reporting/ReportDefinitions/TelerikQuoteReport.Designer.cs
@@ -0,0 +1,193 @@
+namespace JobSystem.Reporting.ReportDefinitions
+{
+	partial class TelerikQuoteReport
+	{
+		#region Component Designer generated code
+		/// <summary>
+		/// Required method for telerik Reporting designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			Telerik.Reporting.Drawing.StyleRule styleRule1 = new Telerik.Reporting.Drawing.StyleRule();
+			Telerik.Reporting.Drawing.StyleRule styleRule2 = new Telerik.Reporting.Drawing.StyleRule();
+			Telerik.Reporting.Drawing.StyleRule styleRule3 = new Telerik.Reporting.Drawing.StyleRule();
+			Telerik.Reporting.Drawing.StyleRule styleRule4 = new Telerik.Reporting.Drawing.StyleRule();
+			this.ReportData = new Telerik.Reporting.ObjectDataSource();
+			this.labelsGroupHeader = new Telerik.Reporting.GroupHeaderSection();
+			this.labelsGroupFooter = new Telerik.Reporting.GroupFooterSection();
+			this.labelsGroup = new Telerik.Reporting.Group();
+			this.pageHeader = new Telerik.Reporting.PageHeaderSection();
+			this.reportNameTextBox = new Telerik.Reporting.TextBox();
+			this.pageFooter = new Telerik.Reporting.PageFooterSection();
+			this.currentTimeTextBox = new Telerik.Reporting.TextBox();
+			this.pageInfoTextBox = new Telerik.Reporting.TextBox();
+			this.reportHeader = new Telerik.Reporting.ReportHeaderSection();
+			this.titleTextBox = new Telerik.Reporting.TextBox();
+			this.reportFooter = new Telerik.Reporting.ReportFooterSection();
+			this.detail = new Telerik.Reporting.DetailSection();
+			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+			// 
+			// ReportData
+			// 
+			this.ReportData.DataMember = "GetQuoteReportData";
+			this.ReportData.DataSource = typeof(JobSystem.Reporting.Data.NHibernate.NHibernateQuoteReportDataProvider);
+			this.ReportData.Name = "ReportData";
+			this.ReportData.Parameters.AddRange(new Telerik.Reporting.ObjectDataSourceParameter[] {
+            new Telerik.Reporting.ObjectDataSourceParameter("quoteId", typeof(System.Guid), null)});
+			// 
+			// labelsGroupHeader
+			// 
+			this.labelsGroupHeader.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.labelsGroupHeader.Name = "labelsGroupHeader";
+			this.labelsGroupHeader.PrintOnEveryPage = true;
+			// 
+			// labelsGroupFooter
+			// 
+			this.labelsGroupFooter.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.labelsGroupFooter.Name = "labelsGroupFooter";
+			this.labelsGroupFooter.Style.Visible = false;
+			// 
+			// labelsGroup
+			// 
+			this.labelsGroup.GroupFooter = this.labelsGroupFooter;
+			this.labelsGroup.GroupHeader = this.labelsGroupHeader;
+			this.labelsGroup.Name = "labelsGroup";
+			// 
+			// pageHeader
+			// 
+			this.pageHeader.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.pageHeader.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.reportNameTextBox});
+			this.pageHeader.Name = "pageHeader";
+			// 
+			// reportNameTextBox
+			// 
+			this.reportNameTextBox.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(0.052916664630174637D), Telerik.Reporting.Drawing.Unit.Cm(0.052916664630174637D));
+			this.reportNameTextBox.Name = "reportNameTextBox";
+			this.reportNameTextBox.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(15.708333015441895D), Telerik.Reporting.Drawing.Unit.Cm(0.60000002384185791D));
+			this.reportNameTextBox.StyleName = "PageInfo";
+			this.reportNameTextBox.Value = "TelerikQuoteReport";
+			// 
+			// pageFooter
+			// 
+			this.pageFooter.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.pageFooter.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.currentTimeTextBox,
+            this.pageInfoTextBox});
+			this.pageFooter.Name = "pageFooter";
+			// 
+			// currentTimeTextBox
+			// 
+			this.currentTimeTextBox.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(0.052916664630174637D), Telerik.Reporting.Drawing.Unit.Cm(0.052916664630174637D));
+			this.currentTimeTextBox.Name = "currentTimeTextBox";
+			this.currentTimeTextBox.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(7.8277082443237305D), Telerik.Reporting.Drawing.Unit.Cm(0.60000002384185791D));
+			this.currentTimeTextBox.StyleName = "PageInfo";
+			this.currentTimeTextBox.Value = "=NOW()";
+			// 
+			// pageInfoTextBox
+			// 
+			this.pageInfoTextBox.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(7.9335417747497559D), Telerik.Reporting.Drawing.Unit.Cm(0.052916664630174637D));
+			this.pageInfoTextBox.Name = "pageInfoTextBox";
+			this.pageInfoTextBox.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(7.8277082443237305D), Telerik.Reporting.Drawing.Unit.Cm(0.60000002384185791D));
+			this.pageInfoTextBox.Style.TextAlign = Telerik.Reporting.Drawing.HorizontalAlign.Right;
+			this.pageInfoTextBox.StyleName = "PageInfo";
+			this.pageInfoTextBox.Value = "=PageNumber";
+			// 
+			// reportHeader
+			// 
+			this.reportHeader.Height = Telerik.Reporting.Drawing.Unit.Cm(2.0529167652130127D);
+			this.reportHeader.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.titleTextBox});
+			this.reportHeader.Name = "reportHeader";
+			// 
+			// titleTextBox
+			// 
+			this.titleTextBox.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(0D), Telerik.Reporting.Drawing.Unit.Cm(0D));
+			this.titleTextBox.Name = "titleTextBox";
+			this.titleTextBox.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(15.814167022705078D), Telerik.Reporting.Drawing.Unit.Cm(2D));
+			this.titleTextBox.StyleName = "Title";
+			this.titleTextBox.Value = "TelerikQuoteReport";
+			// 
+			// reportFooter
+			// 
+			this.reportFooter.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.reportFooter.Name = "reportFooter";
+			// 
+			// detail
+			// 
+			this.detail.Height = Telerik.Reporting.Drawing.Unit.Cm(0.71437495946884155D);
+			this.detail.Name = "detail";
+			// 
+			// TelerikQuoteReport
+			// 
+			this.DataSource = this.ReportData;
+			this.Groups.AddRange(new Telerik.Reporting.Group[] {
+            this.labelsGroup});
+			this.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.labelsGroupHeader,
+            this.labelsGroupFooter,
+            this.pageHeader,
+            this.pageFooter,
+            this.reportHeader,
+            this.reportFooter,
+            this.detail});
+			this.Name = "TelerikQuoteReport";
+			this.PageSettings.Margins.Bottom = Telerik.Reporting.Drawing.Unit.Mm(25.399999618530273D);
+			this.PageSettings.Margins.Left = Telerik.Reporting.Drawing.Unit.Mm(25.399999618530273D);
+			this.PageSettings.Margins.Right = Telerik.Reporting.Drawing.Unit.Mm(25.399999618530273D);
+			this.PageSettings.Margins.Top = Telerik.Reporting.Drawing.Unit.Mm(25.399999618530273D);
+			this.PageSettings.PaperKind = System.Drawing.Printing.PaperKind.A4;
+			this.Style.BackgroundColor = System.Drawing.Color.White;
+			styleRule1.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector("Title")});
+			styleRule1.Style.Color = System.Drawing.Color.Black;
+			styleRule1.Style.Font.Bold = true;
+			styleRule1.Style.Font.Italic = false;
+			styleRule1.Style.Font.Name = "Tahoma";
+			styleRule1.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(20D);
+			styleRule1.Style.Font.Strikeout = false;
+			styleRule1.Style.Font.Underline = false;
+			styleRule2.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector("Caption")});
+			styleRule2.Style.Color = System.Drawing.Color.Black;
+			styleRule2.Style.Font.Name = "Tahoma";
+			styleRule2.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(11D);
+			styleRule2.Style.VerticalAlign = Telerik.Reporting.Drawing.VerticalAlign.Middle;
+			styleRule3.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector("Data")});
+			styleRule3.Style.Font.Name = "Tahoma";
+			styleRule3.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(11D);
+			styleRule3.Style.VerticalAlign = Telerik.Reporting.Drawing.VerticalAlign.Middle;
+			styleRule4.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector("PageInfo")});
+			styleRule4.Style.Font.Name = "Tahoma";
+			styleRule4.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(11D);
+			styleRule4.Style.VerticalAlign = Telerik.Reporting.Drawing.VerticalAlign.Middle;
+			this.StyleSheet.AddRange(new Telerik.Reporting.Drawing.StyleRule[] {
+            styleRule1,
+            styleRule2,
+            styleRule3,
+            styleRule4});
+			this.Width = Telerik.Reporting.Drawing.Unit.Cm(15.814167022705078D);
+			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+		}
+		#endregion
+
+		private Telerik.Reporting.ObjectDataSource ReportData;
+		private Telerik.Reporting.GroupHeaderSection labelsGroupHeader;
+		private Telerik.Reporting.GroupFooterSection labelsGroupFooter;
+		private Telerik.Reporting.Group labelsGroup;
+		private Telerik.Reporting.PageHeaderSection pageHeader;
+		private Telerik.Reporting.TextBox reportNameTextBox;
+		private Telerik.Reporting.PageFooterSection pageFooter;
+		private Telerik.Reporting.TextBox currentTimeTextBox;
+		private Telerik.Reporting.TextBox pageInfoTextBox;
+		private Telerik.Reporting.ReportHeaderSection reportHeader;
+		private Telerik.Reporting.TextBox titleTextBox;
+		private Telerik.Reporting.ReportFooterSection reportFooter;
+		private Telerik.Reporting.DetailSection detail;
+
+	}
+}
