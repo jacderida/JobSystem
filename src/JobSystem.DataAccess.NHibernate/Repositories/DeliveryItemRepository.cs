9c869383f67769554b7de022083ac5f03abdcc83
Chris O'Neil <chriso@gaelquality.com>
2012-03-16 20:33:30 +0000

    Forgot to save csproj!

---
+++ src/JobSystem.DataAccess.NHibernate/Repositories/DeliveryItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using JobSystem.DataModel.Entities;
+using JobSystem.DataModel.Repositories;
+
+namespace JobSystem.DataAccess.NHibernate.Repositories
+{
+	public class DeliveryItemRepository : RepositoryBase<DeliveryItem>, IDeliveryItemRepository
+	{
+	}
+}
