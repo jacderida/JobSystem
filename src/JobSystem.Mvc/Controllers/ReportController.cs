9e188c88a7e4ccc76febb41d1bf5487727cee559
Iain Johnson <iainj@gaelquality.com>
2012-05-21 17:45:49 +0100

    Job Item report
    Added index action/view
    Added job item report viewmodel for selecting status

--- src/JobSystem.Mvc/Controllers/ReportController.cs
+++ src/JobSystem.Mvc/Controllers/ReportController.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Web.Mvc;
+using JobSystem.BusinessLogic.Services;
+using JobSystem.DataModel.Entities;
+using JobSystem.Mvc.Core.Utilities;
+using JobSystem.Mvc.ViewModels.Reports;
 
 namespace JobSystem.Mvc.Controllers
 {
 	public class ReportController : Controller
 	{
-		public ReportController()
+		private readonly ListItemService _listItemService;
+		
+		public ReportController(ListItemService listItemService)
 		{
+			_listItemService = listItemService;
+		}
 
+		public ActionResult Index()
+		{
+			var viewmodel = new JobItemReportViewModel(){
+				Status = _listItemService.GetAllByCategory(ListItemCategoryType.JobItemStatus).ToSelectList()
+			};
+			return View(viewmodel);
 		}
 
 		public ActionResult GenerateEquipmentProgressReport(Guid id)
 		{
 			return View("RepEquipmentProgress", id);
+		}
+
+		public ActionResult GenerateJobItemReport(Guid statusId)
+		{
+			return View();
 		}
 	}
 }
---
+++ src/JobSystem.Mvc/ViewModels/Reports/JobItemReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JobSystem.Mvc.ViewModels.Reports
+{
+	public class JobItemReportViewModel
+	{
+		public Guid StatusId { get; set; }
+		public IEnumerable<SelectListItem> Status { get; set; }
+	}
+}
