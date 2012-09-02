f590c0685d04f4398acf8ae48347411d8c2ba16d
Chris O'Neil <chriso@gaelquality.com>
2012-09-02 22:45:29 +0100

    Build stuff for versioning.

---
+++ build/buildtasks/JobSystem.BuildTasks/ModifyConfigFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JobSystem.BuildTasks
+{
+	public class ModifyConfigFile : Task
+	{
+		[Required]
+		public string ConfigFilePath { get; set; }
+		private XDocument ConfigDocument { get; set; }
+
+		public string JobSystemConnectionString { get; set; }
+
+		public override bool Execute()
+		{
+			ConfigDocument = XDocument.Load(new StringReader(File.ReadAllText(ConfigFilePath)));
+			if (!String.IsNullOrEmpty(JobSystemConnectionString))
+			{
+				var elem = ConfigDocument.Descendants("connectionStrings").Elements().Where(e => e.Attribute("name").Value == "JobSystem").Single();
+				elem.Attribute("connectionString").Value = JobSystemConnectionString;
+			}
+			ConfigDocument.Save(ConfigFilePath);
+			return true;
+		}
+	}
+}
---
+++ build/buildtasks/JobSystem.BuildTasks/UpdateAwsVersionNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace JobSystem.BuildTasks
+{
+	public class UpdateAwsVersionNumber : Task
+	{
+		[Required]
+		public string FilePath { get; set; }
+		[Required]
+		public string Version { get; set; }
+
+		public override bool Execute()
+		{
+			var contents = File.ReadAllText(FilePath);
+			contents = contents.Replace("Application.Version = 0.0.0.0", String.Format("Application.Version = {0}", Version));
+			File.WriteAllText(FilePath, contents);
+			return true;
+		}
+	}
+}
