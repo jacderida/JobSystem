b60869fbcc0aeec520239d4596e9ad5bfb77c1d3
Chris O'Neil <chriso@gaelquality.com>
2012-09-10 04:20:57 +0100

    Removing debugging migration.

---
+++ src/JobSystem.Migrations/M20120910042017_RemoveDebugMigration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentMigrator;
+
+namespace JobSystem.Migrations
+{
+	[Migration(20120910042017)]
+	public class M20120910042017_RemoveDebugMigration : Migration
+	{
+		public override void Up()
+		{
+			Delete.Table("DebuggingTable");
+		}
+
+		public override void Down()
+		{
+			Create.Table("DebuggingTable").WithColumn("Test").AsString(255).Nullable();
+		}
+	}
+}
