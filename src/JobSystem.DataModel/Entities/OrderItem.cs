bd093eb30b294a960c857e5788d2fe011341e775
Chris O'Neil <chriso@gaelquality.com>
2013-06-23 17:18:51 +0100

    Added carriage to order item.

--- src/JobSystem.DataModel/Entities/OrderItem.cs
+++ src/JobSystem.DataModel/Entities/OrderItem.cs
@@ -10,18 +10,27 @@ namespace JobSystem.DataModel.Entities
         public virtual Guid Id { get; set; }
         public virtual Order Order { get; set; }
         public virtual int ItemNo { get; set; }
-        [Required(ErrorMessageResourceName = "EmptyDescription", ErrorMessageResourceType = typeof(OrderItemMessages))]
-        [StringLength(255, ErrorMessageResourceName = "InvalidDescription", ErrorMessageResourceType = typeof(OrderItemMessages))]
+
+        [Required(ErrorMessageResourceName = "EmptyDescription", ErrorMessageResourceType = typeof (OrderItemMessages))]
+        [StringLength(255, ErrorMessageResourceName = "InvalidDescription",
+            ErrorMessageResourceType = typeof (OrderItemMessages))]
         public virtual string Description { get; set; }
+
         public virtual int Quantity { get; set; }
-        [StringLength(50, ErrorMessageResourceName = "InvalidPartNo", ErrorMessageResourceType = typeof(OrderItemMessages))]
+
+        [StringLength(50, ErrorMessageResourceName = "InvalidPartNo",
+            ErrorMessageResourceType = typeof (OrderItemMessages))]
         public virtual string PartNo { get; set; }
-        [StringLength(2000, ErrorMessageResourceName = "InvalidInstructions", ErrorMessageResourceType = typeof(OrderItemMessages))]
+
+        [StringLength(2000, ErrorMessageResourceName = "InvalidInstructions",
+            ErrorMessageResourceType = typeof (OrderItemMessages))]
         public virtual string Instructions { get; set; }
+
         public virtual int DeliveryDays { get; set; }
         public virtual DateTime? DateReceived { get; set; }
         public virtual JobItem JobItem { get; set; }
         public virtual decimal Price { get; set; }
         public virtual bool InvoiceReceived { get; set; }
+        public virtual decimal? Carriage { get; set; }
     }
 }
---
+++ src/JobSystem.Migrations/M20130623015035_AddCarriageColumn.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace JobSystem.Migrations
+{
+    [Migration(20130623015035)]
+    public class M20130623015035_AddCarriageColumn : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("OrderItems").AddColumn("Carriage").AsDecimal().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Carriage").FromTable("OrderItems");
+        }
+    }
+}
